b4934ea81f0f807379b9af81bb609e3ed5f6e96f
Cristian Eigel <cristian.eigel@icloud.com>
2015-08-14 12:45:06 +0200

    Add dry link moethods to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+  def new_button_link(model_class)
+    model_name = model_class.model_name
+    button_link("New #{model_name.name}", new_polymorphic_path(model_name.element), 'btn-info')
+  end
+
+  def edit_button_link(model)
+    button_link('Edit', edit_polymorphic_path(model), 'btn-success')
+  end
+
+  def show_button_link(model)
+    button_link('Show', model, 'btn-success')
+  end
+
+  def back_button_link(model_class)
+    button_link('Back', polymorphic_path(model_class), 'btn-danger')
+  end
+
+  def destroy_button_link(model)
+    button_link('Destroy', model, 'btn-danger', method: :delete, data: { confirm: 'Are you sure?' })
+  end
+
+  def edit_page_bottom_links(model)
+    back_button_link(model.class) + " " +
+    show_button_link(model)
+  end
+
+  def show_page_bottom_links(model)
+    back_button_link(model.class) + " " +
+    edit_button_link(model)
+  end
+  private
+  def button_link(name, path, style, **additional)
+    link_to name, path, role: 'button', class: "btn #{style} btn-xs", **additional
+  end
 end
--- db/migrate/20150813113124_create_mutants.rb
+++ db/migrate/20150813113124_create_mutants.rb
@@ -2,7 +2,6 @@ class CreateMutants < ActiveRecord::Migration
   def change
     create_table :mutants do |t|
       t.string :name
-
       t.timestamps null: false
     end
     add_index :mutants, :name
